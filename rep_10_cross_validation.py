74d87cc804352434ad5cd68bb975191b8f916d8a
zht <2404711390@qq.com>
2022-09-24 15:35:17 +0800

    Code optimization

--- rep_10_cross_validation.py
+++ rep_10_cross_validation.py
@@ -13,6 +13,7 @@ from skorch import NeuralNetClassifier
 from skorch.callbacks import EpochScoring, Initializer, LRScheduler,  Checkpoint
 from skorch.dataset import ValidSplit
 from torch.optim.lr_scheduler import StepLR
+from thop import profile
 
 
 
@@ -47,11 +48,10 @@ def worker(data):
     return signals, labels, RR
 
 
-def load_data(
-        filename="D:\pythonProject\DWT_CNN\ECG-Classification-Using-CNN-and-CWT-master\dataset\\AAMI_new_mitdb_noPre_noNor.pkl"):
+def load_data():
     import pickle
 
-    with open(filename, "rb") as f:
+    with open("db.pkl", "rb") as f:
         train_data = pickle.load(f)
 
     all_train_signals, all_train_labels, all_RR = [], [], []
@@ -70,7 +70,6 @@ def load_data(
     for train_index, test_index in ss.split(all_train_signals, all_train_labels):
         x_train.append(all_train_signals[train_index]), x_test.append(all_train_signals[test_index])
         y_train.append(all_train_labels[train_index]), y_test.append(all_train_labels[test_index])
-        # RR_train.append(train_RR[train_index]),RR_test.append(train_RR[test_index])
         RR_train.append(scaler.fit_transform(all_RR[train_index]))
         RR_test.append(scaler.transform(all_RR[test_index]))
 
@@ -122,8 +121,7 @@ def load_data(
     y_test = np.array(y_test).astype(np.int64)
 
     with open(
-            "D:\pythonProject\DWT_CNN\ECG-Classification-Using-CNN-and-CWT-master\\9_3.pkl",
-            "wb") as f:
+            "10_cross_validation_db.pkl","wb") as f:
         pickle.dump((all_train_s, RR_train, y_train, all_test_s, RR_test, y_test), f, protocol=4)
 
 
@@ -136,11 +134,9 @@ def _conv_bn(input_channel, output_channel, kernel_size=3, padding=1, stride=1, 
 
 
 class RepBlock(nn.Module):
-    def __init__(self, input_channel, output_channel, kernel_size, groups=1, stride=2, deploy=False, padding=1,
-                 use_se=False, flag_3=False):
+    def __init__(self, input_channel, output_channel, kernel_size, groups=1, stride=2, deploy=False, padding=1, flag_3=False):
         super().__init__()
         self.stride = stride
-        self.use_se = use_se
         self.input_channel = input_channel
         self.output_channel = output_channel
         self.deploy = deploy
@@ -153,19 +149,15 @@ class RepBlock(nn.Module):
         if (not self.deploy):
 
             self.brb_5 = _conv_bn(input_channel, output_channel, stride=self.stride, kernel_size=self.kernel_size,
-                                  padding=self.padding,
-                                  groups=groups)
+                                  padding=self.padding,groups=groups)
             self.brb_3 = _conv_bn(input_channel, output_channel, stride=self.stride, kernel_size=kernel_size - 2,
-                                  padding=self.padding - 1,
-                                  groups=groups)
+                                  padding=self.padding - 1,groups=groups)
             if self.flag_3 == True:
                 self.brb_1 = _conv_bn(input_channel, output_channel, stride=self.stride, kernel_size=kernel_size - 4,
-                                      padding=self.padding - 2,
-                                      groups=groups)
+                                      padding=self.padding - 2,groups=groups)
         else:
-            self.brb_rep = nn.Conv1d(in_channels=input_channel, out_channels=output_channel,
-                                     kernel_size=self.kernel_size, padding=self.padding, padding_mode='zeros',
-                                     stride=stride, bias=True)
+            self.brb_rep = nn.Conv1d(in_channels=input_channel, out_channels=output_channel,kernel_size=self.kernel_size,
+                                     padding=self.padding, padding_mode='zeros',stride=stride, bias=True)
 
 
         self.se = nn.Identity()
@@ -252,8 +244,6 @@ class RepBlock(nn.Module):
         return kernel * t, beta - running_mean * gamma / std
 
 
-
-
 class MyModule221(nn.Module):
     def __init__(self):
         super(MyModule221, self).__init__()
@@ -263,7 +253,7 @@ class MyModule221(nn.Module):
         self.p1_2 = nn.MaxPool1d(kernel_size=3, stride=3)
         self.b1_3 = nn.Conv1d(in_channels=16, out_channels=32, kernel_size=4, stride=1, padding=1)
         self.ba1 = nn.BatchNorm1d(32)
-        self.a1 = nn.ReLU()
+        self.r1 = nn.ReLU()
         self.p1_3 = nn.AvgPool1d(kernel_size=3, stride=3)
 
         self.b2_1 = RepBlock(input_channel=1, output_channel=8, kernel_size=7, stride=1, padding=2)
@@ -272,7 +262,7 @@ class MyModule221(nn.Module):
         self.p2_2 = nn.MaxPool1d(kernel_size=3, stride=3)
         self.b2_3 = nn.Conv1d(in_channels=16, out_channels=32, kernel_size=4, stride=1, padding=1)
         self.ba2 = nn.BatchNorm1d(32)
-        self.a2 = nn.ReLU()
+        self.r2 = nn.ReLU()
         self.p2_3 = nn.AvgPool1d(kernel_size=3, stride=3)
 
         self.b3_1 = RepBlock(input_channel=1, output_channel=8, kernel_size=7, stride=1, padding=2)
@@ -281,7 +271,7 @@ class MyModule221(nn.Module):
         self.p3_2 = nn.MaxPool1d(kernel_size=3, stride=3)
         self.b3_3 = nn.Conv1d(in_channels=16, out_channels=32, kernel_size=4, stride=1, padding=1)
         self.ba3 = nn.BatchNorm1d(32)
-        self.a3 = nn.ReLU()
+        self.r3 = nn.ReLU()
         self.p3_3 = nn.AvgPool1d(kernel_size=3, stride=3)
 
         self.fc1 = nn.Linear(260, 128)
@@ -298,7 +288,7 @@ class MyModule221(nn.Module):
         x3 = self.p1_2(x3)
         x3 = self.b1_3(x3)
         x3 = self.ba1(x3)
-        x3 = self.a1(x3)
+        x3 = self.r1(x3)
         x3 = self.p1_3(x3)
 
         x4 = self.b2_1(x4)
@@ -307,7 +297,7 @@ class MyModule221(nn.Module):
         x4 = self.p2_2(x4)
         x4 = self.b2_3(x4)
         x4 = self.ba2(x4)
-        x4 = self.a2(x4)
+        x4 = self.r2(x4)
         x4 = self.p2_3(x4)
 
         x5 = self.b3_1(x5)
@@ -316,7 +306,7 @@ class MyModule221(nn.Module):
         x5 = self.p3_2(x5)
         x5 = self.b3_3(x5)
         x5 = self.ba3(x5)
-        x5 = self.a3(x5)
+        x5 = self.r3(x5)
         x5 = self.p3_3(x5)
 
         x3 = x3.view((1, -1))
@@ -358,10 +348,7 @@ def exaluated(confusion_matrix):
     FN = FN.astype(float)
     TP = TP.astype(float)
     TN = TN.astype(float)
-    # Se = np.average(TP / (TP + FN))
-    # Sp = np.average(TN / (TN + FP))
-    # Tpp = np.average(TP / (TP + FP))
-    # ACC = np.average((TP + TN) / (TP + TN + FP + FN))
+
     m = []
     Se = TP / (TP + FN)
     Sp = TN / (TN + FP)
@@ -375,47 +362,35 @@ def exaluated(confusion_matrix):
 
 
 def loading(i):
-    with open(
-            "db.pkl",
-            "rb") as f:
+    with open("10_cross_validation_db.pkl","rb") as f:
         all_train_s, RR_train, y_train, all_test_s, RR_test, y_test = pickle.load(f)
 
-    x3 = all_train_s[i][0]
-    x4 = all_train_s[i][1]
-    x5 = all_train_s[i][2]
+    train_3 = all_train_s[i][0]
+    train_4 = all_train_s[i][1]
+    train_5 = all_train_s[i][2]
 
-    x9 = np.array(RR_train[i]).astype(np.float32)
+    train_RR = np.array(RR_train[i]).astype(np.float32)
     y_t = y_train[i]
 
 
-    x33 = all_test_s[i][0]
-    x44 = all_test_s[i][1]
-    x55 = all_test_s[i][2]
+    test_3 = all_test_s[i][0]
+    test_4 = all_test_s[i][1]
+    test_5 = all_test_s[i][2]
 
-    x99 = np.array(RR_test[i]).astype(np.float32)
+    test_RR = np.array(RR_test[i]).astype(np.float32)
     y_te = y_test[i]
 
-    return x3, x4, x5, x9, y_t, x33, x44, x55, x99, y_te
+    return train_3, train_4, train_5, train_RR, y_t, test_3, test_4, test_5, test_RR, y_te
 
 
 def test_moudle():
     for i in range(10):
-
         callbacks = [
             Initializer("[conv|fc]*.weight", fn=torch.nn.init.kaiming_normal_),
             Initializer("[conv|fc]*.bias", fn=partial(torch.nn.init.constant_, val=0.0)),
             LRScheduler(policy=StepLR, step_size=5, gamma=0.1),
-            EpochScoring(scoring=make_scorer(f1_score, average="macro"), lower_is_better=False, name="valid_f1"),
-            Checkpoint(monitor='valid_acc_best',
-                       f_params="/checkpoint/params/" + str(
-                           i) + "_params.pt",
-                       f_history="/checkpoint/history/" + str(
-                           i) + "_history.json",
-                       f_criterion="/checkpoint/criterion/" + str(
-                           i) + "_criterion.pt",
-                       f_optimizer="/checkpoint/optimizer/" + str(
-                           i) + "_optimizer.pt")
-        ]
+            EpochScoring(scoring=make_scorer(f1_score, average="macro"), lower_is_better=False, name="valid_f1"),\
+            ]
         net = NeuralNetClassifier(
             module=MyModule221,
             criterion=nn.CrossEntropyLoss,
@@ -495,7 +470,6 @@ def test_moudle():
             for j in range(5):
                 for k in range(5):
                     matrix[j][k] = matrix[j][k] + m1[j][k]
-            j, k = 0, 0
             for j in range(4):
                 for k in range(5):
                     score[j][k] = score[j][k] + s1[j][k]
@@ -525,8 +499,48 @@ def test_moudle():
         print("混淆矩阵之和：")
         print(matrix)
 
+
+def rep():
+    callbacks = [
+        Initializer("[conv|fc]*.weight", fn=torch.nn.init.kaiming_normal_),
+        Initializer("[conv|fc]*.bias", fn=partial(torch.nn.init.constant_, val=0.0)),
+        LRScheduler(policy=StepLR, step_size=17, gamma=0.1),
+        EpochScoring(scoring=make_scorer(f1_score, average="macro"), lower_is_better=False, name="valid_f1"),
+        Checkpoint(monitor='valid_acc_best')
+    ]
+    net = NeuralNetClassifier(
+        module=MyModule221,
+        criterion=nn.CrossEntropyLoss,
+        optimizer=torch.optim.Adam,
+        lr=0.001,
+        max_epochs=40,
+        batch_size=150,
+        train_split=ValidSplit(0.2),
+        verbose=1,
+        device="cpu",
+        callbacks=callbacks,
+    )
+
+    net.initialize()
+    net.load_params(f_params='1_params.pt', )
+    module = net.module()
+    module.eval()
+
+    x3, x4, x5, x9 = torch.randn(150, 1, 256), torch.randn(150, 1, 256), torch.randn(150, 1, 256), torch.randn(150, 4)
+    macs1, params1 = profile(module, inputs=(x3, x4, x5, x9))
+    print("重参数前的计算量：" + str(macs1) + "参数量：" + str(params1))
+
+    module.switch()
+    macs2, params2 = profile(module, inputs=(x3, x4, x5, x9))
+    print("重参数后的计算量：" + str(macs2) + "参数量：" + str(params2))
+
+
+
+
 if __name__ == '__main__':
     # load_data()
+    # rep()
+
 
     acc, matrix, score = [], [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]], [
         [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
@@ -535,7 +549,7 @@ if __name__ == '__main__':
         callbacks = [
             Initializer("[conv|fc]*.weight", fn=torch.nn.init.kaiming_normal_),
             Initializer("[conv|fc]*.bias", fn=partial(torch.nn.init.constant_, val=0.0)),
-            LRScheduler(policy=StepLR, step_size=5, gamma=0.1),
+            LRScheduler(policy=StepLR, step_size=12, gamma=0.1),
             EpochScoring(scoring=make_scorer(f1_score, average="macro"), lower_is_better=False, name="valid_f1"),
             Checkpoint(monitor='valid_acc_best',
                        f_params="/checkpoint/params/" + str(
@@ -556,7 +570,7 @@ if __name__ == '__main__':
             batch_size=150,
             train_split=ValidSplit(0.2),
             verbose=1,
-            device="cuda:3",
+            device="cpu",
             callbacks=callbacks,
         )
 
